636afe0ceb147f3c69bb598dc3948f20edd36f8a
Rolly Maulana Awangga <rolly@awang.ga>
2022-12-09 08:36:11 +0700

    oke js croot

--- docs/js/geojson.js
+++ docs/js/geojson.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([4.694012785431503, 96.7750300832872], 13);
+const map = L.map('map').setView([-6.87360695863978, 107.5759227360176], 23);
 
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
@@ -6,21 +6,9 @@ const map = L.map('map').setView([4.694012785431503, 96.7750300832872], 13);
 	}).addTo(map);
 
 
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
-	const desaLayer = L.geoJSON(my_JSON_object, {
+
+
+
+	L.geoJSON(getGeoJSON("bandung/ulbi.json"), {
 		onEachFeature: onEachFeature
 	}).addTo(map);
-
-
-
-
-	var request = new XMLHttpRequest();
-	request.open("GET", "bandung/ulbi.json", false);
-	request.send(null)
-	var my_JSON_object = JSON.parse(request.responseText);
-	console.log(my_JSON_object);
