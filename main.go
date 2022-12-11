1128941e730ecaf72ef1030d7f8f6d555313518e
Rolly Maulana Awangga <rolly@awang.ga>
2022-12-12 06:30:55 +0700

    set dinamic port

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+SetPort(), nil)
+}
+
+func SetPort() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "80"
+	}
+	return port
 }
