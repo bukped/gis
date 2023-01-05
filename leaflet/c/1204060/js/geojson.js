2b013344c3ddfe226a50e752d62fd5a3caf08a10
Valen Rionald <vrionald@gmail.com>
2023-01-05 14:39:16 +0700

    Add Bekasi Leaflet

--- leaflet/c/1204060/js/geojson.js
+++ leaflet/c/1204060/js/geojson.js
@@ -1,7 +1,7 @@
-const map = L.map('map').setView([-6.87360695863978, 107.5759227360176], 23);
+const map = L.map('map').setView([-6.215558062747675, 107.12331674775365], 23);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 10,
     attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
 }).addTo(map);
 
