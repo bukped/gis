ebcee3a5f115ce021023ad67482b9cc3c8418790
hanandestiarin <79676860+hanandestiarin@users.noreply.github.com>
2023-01-06 13:25:58 +0700

    tugas 10

---
+++ leaflet/a/1204022/js/geojson.js
@@ -0,0 +1,14 @@
+const map = L.map('map').setView([-7.6746436,110.7915398], 23);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
+
+
+
+
+
+L.geoJSON(getGeoJSON("sukoharjo/sukoharjo.geojson"), {
+    onEachFeature: onEachFeature
+}).addTo(map);
