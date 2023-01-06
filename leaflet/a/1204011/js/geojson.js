266ccd6c515deedc1e92b5ac419cb3c615ec8232
Wildan Azril Arvany <80764322+azrilarva21@users.noreply.github.com>
2023-01-06 13:36:06 +0700

    tugas 10

---
+++ leaflet/a/1204011/js/geojson.js
@@ -0,0 +1,15 @@
+
+const map = L.map('map').setView([-7.3286487,108.2938724], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
+
+
+
+
+
+L.geoJSON(getGeoJSON("ciamis/ciamis.json"), {
+    onEachFeature: onEachFeature
+}).addTo(map);
