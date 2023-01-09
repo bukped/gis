02248fcbbd94176fd96cd9c98e99f9859c3c7c1b
fauziahhennihasibuann <114493159+fauziahhennihasibuann@users.noreply.github.com>
2023-01-09 14:07:23 +0700

    tugas 10

---
+++ leaflet/a/1204013/js/geojson.js
@@ -0,0 +1,15 @@
+
+const map = L.map('map').setView([-7.3262517,110.4558855], 23);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
+
+
+
+
+
+L.geoJSON(getGeoJSON("bandung/ulbi.json"), {
+    onEachFeature: onEachFeature
+}).addTo(map);
