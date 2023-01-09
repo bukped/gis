40ed5ee80aa58f33fde134ea76f08a37372cf820
fauziahhennihasibuann <114493159+fauziahhennihasibuann@users.noreply.github.com>
2023-01-09 15:16:43 +0700

    tugas 10

--- leaflet/a/1204013/js/geojson.js
+++ leaflet/a/1204013/js/geojson.js
@@ -1,5 +1,5 @@
 
-const map = L.map('map').setView([-7.3724807,108.4950044], 23);
+const map = L.map('map').setView([-7.3724807, 108.4950044], 13);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
