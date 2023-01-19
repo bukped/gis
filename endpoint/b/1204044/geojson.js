20798be8cfe9d92e27bf545275bec7f70415ee56
fahiraaa06 <fahira1678@gmail.com>
2023-01-19 22:10:30 +0700

    11-3B-1204044-FAHIRA

--- endpoint/b/1204044/geojson.js
+++ endpoint/b/1204044/geojson.js
@@ -1,9 +1,8 @@
-const map = L.map('map').setView([3.594734611258944, 98.6710254771208], 13);
+const map = L.map('map').setView([3.594734611258944, 98.6710254771208], 10);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
 }).addTo(map);
-
 
 
