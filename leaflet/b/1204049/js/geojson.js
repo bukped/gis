6254bfe44fbbee13d6ebfa7d4aa655079bf95050
fahiraaa06 <fahira1678@gmail.com>
2023-01-19 12:20:27 +0700

    add folder kelas b untuk chapter 4

--- leaflet/b/1204049/js/geojson.js
+++ leaflet/b/1204049/js/geojson.js
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
 const map = L.map('map').setView([-7.874361339282246, 110.3277259173113], 12);
-=======
-const map = L.map('map').setView([-7.876401847326076, 110.32223275304646], 12);
->>>>>>> f64a9d4c884ed73b5f9507d5c2017461184cd413
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
