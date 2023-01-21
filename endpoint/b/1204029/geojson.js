558f1f82384bef3ded9944e65e5d19d81a12b823
fadilfebriansyah <fadilfebriansyah76@gmail.com>
2023-01-21 20:46:08 +0700

    11-3B-1204029-FadilFebriansyah

---
+++ endpoint/b/1204029/geojson.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([1.7364841515544196, 98.78603442916638], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
