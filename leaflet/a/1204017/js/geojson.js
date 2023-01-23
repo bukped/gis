51f2ea42429af6a972792d734c28a34368cc9096
S O E R A H M A T _ ^ <90734813+soerahmat69@users.noreply.github.com>
2023-01-23 16:38:00 +0800

    baru

---
+++ leaflet/a/1204017/js/geojson.js
@@ -0,0 +1,12 @@
+const map = L.map('map').setView([-3.00495,120.19258], 12);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 13,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
+
+
+
+L.geoJSON(getGeoJSON('palopo/palopo.geojson'), {
+    onEachFeature: onEachFeature
+}).addTo(map);
