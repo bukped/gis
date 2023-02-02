9aea86516ba978970e3558adfab538c85669bb60
fauziahhennihasibuann <114493159+fauziahhennihasibuann@users.noreply.github.com>
2023-02-02 09:15:11 +0700

    11-3A-1204013-Fauziah henni

---
+++ endpoint/a/1204013/geojson.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([-7.3724807, 108.4950044], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
