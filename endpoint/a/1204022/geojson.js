341611cedd7cee548c3fbac37c3197f8d93dac2f
hanandestiarin <79676860+hanandestiarin@users.noreply.github.com>
2023-02-02 09:01:06 +0700

    11

---
+++ endpoint/a/1204022/geojson.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([-1.1742562,116.7016594], 23);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
