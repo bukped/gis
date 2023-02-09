d23e79717935560b306731ddfc5b20ea5491273c
Billy R <119595254+billyulbi@users.noreply.github.com>
2023-02-09 21:20:48 +0700

    Create script.js

---
+++ endpoint/rpl/714222072/script.js
@@ -0,0 +1,20 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "api-key": "Sa9Jtl0xGx9HnXnRV7ispcHOz9JdNEYD2rKVGNGICDRmvZqwX8HArSKO6EkAPlEm",
+  "long": 107.575902278736,
+  "lat": -6.8732786951873255
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-mfpjl/endpoint/geojson", requestOptions)
+  .then(response => $('#result').html(response.text()))
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
