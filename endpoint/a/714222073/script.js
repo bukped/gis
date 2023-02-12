415f66fb74e5cd9365d9871e716f9339d739d87e
DiniLubis <119611222+DiniLubis@users.noreply.github.com>
2023-02-12 10:28:38 +0700

    11-RPL-714222073-Ardini Yuanita Lubis

---
+++ endpoint/a/714222073/script.js
@@ -0,0 +1,67 @@
+var Long;
+var Lat;
+
+function myFunction() {
+    var inputLong = document.querySelector("#long").value;
+    var inputLat = document.querySelector("#lat").value;
+    console.log(inputLong);
+    console.log(inputLat);
+    Long = parseFloat(inputLong);
+    Lat = parseFloat(inputLat);
+    document.getElementById("hasil").innerHTML = panggil(Long, Lat); 
+}
+
+function panggil(long, lat) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "api-key": "MKOas3ve0z5lFsL8O92oNrSwpr3ERQ9crvO3207OeROovZtHS2IvNtoFzGcVqnpK",
+        "long": long,
+        "lat": lat
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://asia-south1.gcp.data.mongodb-api.com/app/geojson-fdfog/endpoint/geojson", requestOptions)
+        .then(response => response.text())
+        .then(result => hasil(result))
+        .catch(error => console.log('error', error));
+
+    fetch("https://asia-south1.gcp.data.mongodb-api.com/app/geojson-fdfog/endpoint/geojson", requestOptions)
+        .then(response => response.text())
+        .then(result => geo(JSON.parse(result)))
+        .catch(error => console.log('error', error));
+
+}
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+function geo(hasil) {
+    L.geoJSON(hasil, {
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+}
+
+function hasil(result) {
+    document.getElementById("hasil").innerHTML = result;
+
+    L.marker([Lat, Long]).addTo(map);
+
+    var popup = L.popup([Lat, Long], { content: result })
+        .openOn(map);
+
+    map.setView([Lat, Long], 13);
+
+}
