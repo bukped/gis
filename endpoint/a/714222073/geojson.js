41c35e95984f30c026d5337556fe4da9424ebcac
DiniLubis <119611222+DiniLubis@users.noreply.github.com>
2023-02-12 10:25:01 +0700

    11-RPL-714222073-Ardini Yuanita Lubis

---
+++ endpoint/a/714222073/geojson.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([4.695135, 96.749397], 12);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.bukupedia.co.id/">Bukupedia University</a>'
+}).addTo(map);
